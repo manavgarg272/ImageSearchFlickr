f53ac3e23213511660f2684713e806392b082006
manav garg <manavgarg272@gmail.com>
2021-08-01 23:42:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import Layout from "./Containers/Layout/Layout";
+import Images from "./images";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      id="appMain"
+      style={{
+        height: "100%",
+        width: "100%",
+        marginTop: "0px",
+      }}
+    >
+      <Images />
     </div>
   );
 }
---
+++ src/images.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Fragment } from "react";
+import "./images.css";
+import history from "./history";
+
+function ImagesRecommended() {
+  // e.preventDefault();
+
+  const [Images, setImages] = useState([]);
+  const [inputText, setinputText] = useState("");
+  const getImages = async (e) => {
+    const response = await fetch(
+      "https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=fa069a72b6c8974da3062a83340641e6&format=json&nojsoncallback=1"
+    );
+    const data = await response.json();
+    setImages(data.photos.photo);
+  };
+
+  const inputEvent = async (e) => {
+    await setinputText(e.target.value);
+  };
+
+  const onSubmit = async (e) => {
+    try {
+      var url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=fa069a72b6c8974da3062a83340641e6&text=${inputText}&format=json&nojsoncallback=1`;
+      await axios.get(url).then((response) => {
+        setImages(response.data.photos.photo);
+      });
+    } catch {
+      history.push("/");
+    }
+  };
+
+  useEffect((e) => {
+    getImages(e);
+  }, []);
+
+  return (
+    <Fragment>
+      <div className="ImagesRecommended">
+        <div className="header">
+          <p>ImageSearch</p>
+          <input
+            type="text"
+            className="inputBar"
+            onChange={inputEvent}
+            placeholder="Search Images"
+          />
+          <button onClick={onSubmit} className="Button">
+            Search
+          </button>
+        </div>
+
+        <div className="showimages">
+          {Images.map((i, key) => {
+            console.log(i);
+            return (
+              <img
+                key={key}
+                src={
+                  "https://farm" +
+                  i.farm +
+                  ".staticflickr.com/" +
+                  i.server +
+                  "/" +
+                  i.id +
+                  "_" +
+                  i.secret +
+                  ".jpg"
+                }
+                alt=""
+              />
+            );
+          })}
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+export default ImagesRecommended;
